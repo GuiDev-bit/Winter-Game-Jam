2bf44e164f6f3266b9b24debe74b0fe405ee0626
Juicymaneadj <adjaym23@ecolecatholique.ca>
2026-02-22 01:22:56 -0500

    J'ai crée la state machine du joueur, j'ai ajouter 3 sprites du joueur, J'ai un peu plus améliorer les déplacements , le saut et la gravité pour les entités. j'ai rajouté un health compenent

--- script/component/input_component.gd
+++ script/component/input_component.gd
@@ -9,7 +9,7 @@ func _ready() -> void:
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _physics_process(delta: float) -> void:
+func _physics_process(_delta: float) -> void:
 	x_input = Input.get_axis("left", "right")
 	jump_press = Input.is_action_just_pressed("jump")
 	
--- script/player/player.gd
+++ script/player/player.gd
@@ -1,20 +1,62 @@
 extends CharacterBody2D
+class_name Player
+
 @onready var  move_component : MovementComponement=$MovementComponent
 @onready var  input_component : Inputcomponent =$InputComponent
 
+#states
+enum STATE { FALL, FLOOR,JUMP, HURT, }
+
+var active_state :STATE = STATE.FALL  
+
+func _ready() -> void:
+	switch_state(active_state)
 
 func _physics_process(delta) -> void :
-	move_component.direction = input_component.x_input #assigner la directiomn au mouvement compp
-	move_component.slide(delta)
-	if not  is_on_floor() : 
-		move_component.apply_gravity(delta)
-
+	move_component.direction = input_component.x_input #assigner la directiomn au mouvement comp
+	process_state(delta)
 	move_and_slide()
-#Je vais tout refaire en state machine
 
-func active_game():  #j'ai séparé ca du joueur
+
+func active_game():  #check if game is active 
 	if GameManager.current_state != GameManager.GameState.PLAYING:
 		velocity = Vector2.ZERO
 		move_and_slide()
 		return
-	
+
+#state Transition
+func switch_state(to_state : STATE) :
+	active_state = to_state
+
+	#State specific thing that will run once when entering the state
+	match  active_state :
+		STATE.FALL :
+			pass
+		STATE.JUMP :
+			move_component.jump()
+			if input_component.x_input != 0:
+				velocity.x = 500 * input_component.x_input
+
+
+func process_state(delta: float) ->void : #handle state-logique
+	match active_state :
+		STATE.FALL :
+			move_component.apply_gravity(delta, 1.3)
+			move_component.air_slide(delta)
+			if is_on_floor() :
+				switch_state(STATE.FLOOR)
+
+		STATE.FLOOR:
+			if not is_on_floor() :
+				switch_state(STATE.FALL)
+			move_component.slide(delta)
+			if input_component.jump_press :
+				switch_state(STATE.JUMP)
+
+
+
+		STATE.JUMP :
+			move_component.apply_gravity(delta)
+			move_component.slide(delta)
+			if velocity.y >=   0 : 
+				switch_state(STATE.FALL)
