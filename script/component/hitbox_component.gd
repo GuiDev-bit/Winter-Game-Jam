3babe3eb5b6801b28092adbd2a0ec9a5a9860e5e
Juicymaneadj <adjaym23@ecolecatholique.ca>
2026-02-22 02:19:06 -0500

    créer la template pour le syteme de hurbox et hitbox , ainsi qu'une ressource pour les attaques

---
+++ script/component/hitbox_component.gd
@@ -0,0 +1,21 @@
+extends Area2D
+class_name  HitboxComponent
+
+@export var attack_data : = 0
+@export var shape : CollisionObject2D 
+@export var team := 0
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+func lunch_attack() :
+	pass
+
+func end_attack(): 
+	pass
+
+
+func _on_area_entered(area: Area2D) -> void:
+	pass # Replace with function body.
