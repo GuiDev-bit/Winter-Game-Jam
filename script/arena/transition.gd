e2d438c014fd86c0a1098b0622b5d6e82cd25fd1
GuiDev-bit <g.higel@assomptioncolmar.fr>
2026-02-26 20:21:36 +0100

    transition

--- script/arena/transition.gd
+++ script/arena/transition.gd
@@ -6,6 +6,8 @@ var anim_player : AnimationPlayer
 func _ready() -> void:
 	layer = 100
 	rect = ColorRect.new()
+	rect.name = "ColorRect"
+	rect.unique_name_in_owner = true
 	rect.color = Color(0, 0, 0, 1)
 	rect.set_anchors_preset(Control.PRESET_FULL_RECT)
 	rect.visible = false
@@ -19,14 +21,14 @@ func _create_animations() -> void:
 	var fade_out = Animation.new()
 	fade_out.length = 0.4
 	var track = fade_out.add_track(Animation.TYPE_VALUE)
-	fade_out.track_set_path(track, NodePath("../ColorRect:color"))
+	fade_out.track_set_path(track, "%ColorRect:color")
 	fade_out.track_insert_key(track, 0.0, Color(0, 0, 0, 0))
 	fade_out.track_insert_key(track, 0.4, Color(0, 0, 0, 1))
 	lib.add_animation("fade_out", fade_out)
 	var fade_in = Animation.new()
 	fade_in.length = 0.4
 	var track2 = fade_in.add_track(Animation.TYPE_VALUE)
-	fade_in.track_set_path(track2, NodePath("../ColorRect:color"))
+	fade_in.track_set_path(track2, "%ColorRect:color")
 	fade_in.track_insert_key(track2, 0.0, Color(0, 0, 0, 1))
 	fade_in.track_insert_key(track2, 0.4, Color(0, 0, 0, 0))
 	lib.add_animation("fade_in", fade_in)
