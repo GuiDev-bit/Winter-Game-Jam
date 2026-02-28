310e6233b7a63d4c0928242b60a14ce6ede451ba
Juicymaneadj <adjaym23@ecolecatholique.ca>
2026-02-28 01:48:24 -0500

    background

--- script/projectile/snowball.gd
+++ script/projectile/snowball.gd
@@ -7,7 +7,7 @@ var force := 2000
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass
+	print("je suis la")
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
